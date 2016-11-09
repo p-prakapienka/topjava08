21cb947e4cb30a90afb800bb61e2e5976b82ac10
restrictor911 <ciulen666@gmail.com>
2016-11-09 20:17:48 +0300

    HW5_optional_fix_servlet_springmain

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import ru.javawebinar.topjava.Profiles;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.util.TimeUtil;
 import ru.javawebinar.topjava.web.meal.MealRestController;
@@ -33,7 +34,10 @@ public class MealServlet extends HttpServlet {
     @Override
     public void init(ServletConfig config) throws ServletException {
         super.init(config);
-        springContext = new ClassPathXmlApplicationContext("spring/spring-app.xml", "spring/spring-db.xml");
+        String[] configLocations = {"spring/spring-app.xml", "spring/spring-db.xml"};
+        springContext = new ClassPathXmlApplicationContext(configLocations, false);
+        springContext.getEnvironment().setActiveProfiles(Profiles.ACTIVE_DB, Profiles.DATAJPA);
+        springContext.refresh();
         mealController = springContext.getBean(MealRestController.class);
     }
 
--- src/test/java/ru/javawebinar/topjava/SpringMain.java
+++ src/test/java/ru/javawebinar/topjava/SpringMain.java
@@ -2,6 +2,7 @@ package ru.javawebinar.topjava;
 
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
 import ru.javawebinar.topjava.to.MealWithExceed;
 import ru.javawebinar.topjava.web.meal.MealRestController;
 import ru.javawebinar.topjava.web.user.AdminRestController;
@@ -18,19 +19,25 @@ import java.util.List;
  */
 public class SpringMain {
     public static void main(String[] args) {
-        // java 7 Automatic resource management
-        try (ConfigurableApplicationContext appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml","spring/mock.xml")) {
-            System.out.println("Bean definition names: " + Arrays.toString(appCtx.getBeanDefinitionNames()));
-            AdminRestController adminUserController = appCtx.getBean(AdminRestController.class);
-            adminUserController.create(UserTestData.USER);
-            System.out.println();
+        String[] configLocations = {"spring/spring-app.xml", "spring/spring-db.xml"};
+        ConfigurableApplicationContext appCtx = new ClassPathXmlApplicationContext(configLocations, false);
+        appCtx.getEnvironment().addActiveProfile(Profiles.ACTIVE_DB);
+        appCtx.getEnvironment().addActiveProfile(Profiles.DATAJPA);
+        appCtx.refresh();
 
-            MealRestController mealController = appCtx.getBean(MealRestController.class);
-            List<MealWithExceed> filteredMealsWithExceeded =
-                    mealController.getBetween(
-                            LocalDate.of(2015, Month.MAY, 30), LocalTime.of(7, 0),
-                            LocalDate.of(2015, Month.MAY, 31), LocalTime.of(11, 0));
-            filteredMealsWithExceeded.forEach(System.out::println);
-        }
+        System.out.println("Bean definition names: " + Arrays.toString(appCtx.getBeanDefinitionNames()));
+        AdminRestController adminUserController = appCtx.getBean(AdminRestController.class);
+        adminUserController.delete(UserTestData.USER_ID);
+        adminUserController.create(UserTestData.USER);
+        System.out.println();
+
+        MealRestController mealController = appCtx.getBean(MealRestController.class);
+        List<MealWithExceed> filteredMealsWithExceeded =
+                mealController.getBetween(
+                        LocalDate.of(2015, Month.MAY, 30), LocalTime.of(7, 0),
+                        LocalDate.of(2015, Month.MAY, 31), LocalTime.of(11, 0));
+        filteredMealsWithExceeded.forEach(System.out::println);
+
+        appCtx.close();
     }
 }
